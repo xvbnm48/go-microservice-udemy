d445e11126703bbf62df47d38a2e73170cccec6d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-04 22:16:39 +0700

    make env

--- app/app.go
+++ app/app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/xvbnm48/go-microservice-udemy/domain"
@@ -21,14 +22,16 @@ func Start() {
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 	// router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 	// router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
 
-func getCustomer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fmt.Fprint(w, vars["customer_id"])
-}
+// func getCustomer(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	fmt.Fprint(w, vars["customer_id"])
+// }
 
 // func createCustomer(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprint(w, "post request received")
