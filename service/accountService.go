09e9d13e3c8f9815ae09e77b8d37d9b899664031
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-05 22:08:18 +0700

    account service

---
+++ service/accountService.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"time"
+
+	"github.com/xvbnm48/go-microservice-udemy/domain"
+	"github.com/xvbnm48/go-microservice-udemy/dto"
+	"github.com/xvbnm48/go-microservice-udemy/errs"
+)
+
+type AccountService interface {
+	NewAccount(dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError)
+}
+
+type DefaultAccountService struct {
+	repo domain.AccountRepository
+}
+
+func (s DefaultAccountService) NewAccount(req dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError) {
+	a := domain.Account{
+		AccoutnId:   "",
+		CustomerId:  req.CustomerId,
+		OpeningDate: time.Now().Format("2006-01-02T15:04:05Z07:00"),
+		AccountType: req.AccountType,
+		Amount:      req.Amount,
+		Status:      "1",
+	}
+	newAccount, err := s.repo.Save(a)
+	if err != nil {
+		return nil, err
+	}
+
+	response := newAccount.ToNewAccountResponseDto()
+	return &response, nil
+}
+
+func NewAccountService(repo domain.AccountRepository) DefaultAccountService {
+	return DefaultAccountService{repo: repo}
+}
